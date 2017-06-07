32cf73226b9c581373bf14061b8bf9ab1e6e32fc
eldervaz <eldervaz@gmail.com>
2017-06-06 19:40:01 -0500

    * .gitignore: first Commit from xamarin for mac
    * Main.cs:
    * tiendaMKT.sln:
    * Info.plist:
    * App.xaml:
    * AppDelegate.cs:
    * packages.config:
    * Product.cs:
    * App.xaml.cs:
    * MainActivity.cs:
    * packages.config:
    * Entitlements.plist:
    * tiendaMKT.iOS.csproj:
    * packages.config:
    * tiendaMKT.csproj:
    * fotoDemo.jpg:
    * LaunchScreen.storyboard:
    * tiendaMKTPage.xaml:
    * AboutAssets.txt:
    * tiendaMKT.Droid.csproj:
    * tiendaMKTPage.xaml.cs:
    * AssemblyInfo.cs:
    * icon.png:
    * styles.xml:
    * Contents.json:
    * Tabbar.axml:
    * AboutResources.txt:
    * Toolbar.axml:
    * AndroidManifest.xml:
    * AssemblyInfo.cs:
    * Resource.designer.cs:
    * icon.png:
    * icon.png:
    * icon.png:
    * Contents.json:

---
+++ tiendaMKT/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace tiendaMKT
+{
+    class Product
+    {
+        public string nombre { get; set; }   
+        public string cantidad { get; set; }
+
+		public string modelo { get; set; }
+		public string codigo { get; set; }
+		public string imagen { get; set; }
+		public string instructor { get; set; }
+		public int pCiento { get; set; }
+		public int pMillar { get; set; }
+        public Array colores { get; set; }
+    }
+}
---
+++ tiendaMKT/tiendaMKTPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using Xamarin.Forms;
+
+namespace tiendaMKT
+{
+    public partial class tiendaMKTPage : ContentPage
+    {
+
+
+        JArray arrCourse;
+
+
+
+        public tiendaMKTPage()
+        {
+            InitializeComponent();
+            Padding = Device.OnPlatform(new Thickness(0, 20, 0, 0), new Thickness(0), new Thickness(0));
+            loadJSON();
+        }
+
+        private async void loadJSON()
+        {
+			var client = new HttpClient()
+			{
+				BaseAddress = new Uri("http://area51.pe/sol/")
+			};
+			string url = string.Format("tienda.json");
+			var resp = await client.GetAsync(url);
+			var result = resp.Content.ReadAsStringAsync().Result;
+
+			JObject values = JObject.Parse(result);
+
+			arrCourse = (JArray)values["tienda"];
+
+            Debug.WriteLine("fin lectura JSON");
+
+			FillData(arrCourse);
+        }
+
+        private void FillData(JArray arrCourse)
+        {
+            var people = new List<Product>();
+
+            for (int i = 0; i < arrCourse.Count; i++){
+
+                string cant = (((JArray)arrCourse[i]["items"]).Count).ToString();
+
+                Debug.WriteLine(  cant   );
+
+                Product tmp  = new Product { nombre = arrCourse[i]["nombre"].ToString(), 
+                                            imagen = "http://area51.pe/sol/" + arrCourse[i]["imagen"].ToString(),
+                                            cantidad = cant
+				};
+
+                Debug.WriteLine(cant);
+                Debug.WriteLine(tmp.imagen);
+
+                people.Add( tmp );
+
+                Debug.WriteLine( tmp.imagen );
+            }
+
+
+            list.ItemsSource = people;
+        }
+    }
+}
